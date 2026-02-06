b88a0c952a41431b0833a828a27014aeac12e83e
Igor Morozov <street313@icloud.com>
2026-02-06 16:13:05 +0300

    HW4_a18

---
+++ HW4/a18.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    scanf("%d%d",&a,&b);
+    if(a>b)
+       printf("Above");
+    else if(a<b)
+       printf("Less");
+    else if(a==b)
+       printf("Equal");     
+    return 0;
+}
